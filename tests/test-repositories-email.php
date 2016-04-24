76de2c80e0d6ac2ef750595671fc52bc73efff1d
Aron Marriott-Smith <aron@atomace.com>
2016-04-24 12:41:14 +0100

    [Add] Added test for send method

--- tests/test-repositories-email.php
+++ tests/test-repositories-email.php
@@ -24,4 +24,32 @@ class Drafty_Repositories_Email_Test extends Drafty_UnitTestCase {
 		$res = Email::validate( 'name@example.com', 'My subject', null );
 		$this->assertInstanceOf( 'WP_Error', $res );
 	}
+
+	/**
+	 * @covers Email::send
+	 */
+	public function test_send_returns_true_when_we_pass_valid_parameters()
+	{
+		$to = get_option('admin_email');
+		$res = Email::to($to)->subject('My Subject')->text('My Message')->HTML('<p>Some HTML</p>')->send();
+		$this->assertTrue( $res );
+
+		$res = Email::to($to)->subject('My Subject')->text('My Message')->HTML(null)->send();
+		$this->assertTrue( $res );
+
+		$res = Email::to($to)->subject('My Subject')->text(null)->HTML('<p>Some HTML</p>')->send();
+		$this->assertTrue( $res );
+
+		// invalid email
+		$res = Email::to('example.com')->subject('My Subject')->text('My Message')->HTML('<p>Some HTML</p>')->send();
+		$this->assertFalse( $res );
+		
+		// no subject
+		$res = Email::to($to)->subject(null)->text('My Message')->HTML('<p>Some HTML</p>')->send();
+		$this->assertFalse( $res );
+
+		// no message text or HTML
+		$res = Email::to($to)->subject('My Subject')->text(null)->HTML(null)->send();
+		$this->assertFalse( $res );
+	}
 }
